2041a69dbd871b0710fcd2dbc9c5878f163faada
shufflesninja <isaact23@vt.edu>
2021-09-14 13:11:53 -0400

    Add react-color module

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,7 +1,10 @@
+import { HuePicker } from 'react-color'
+
 export default function ColorPicker() {
     return (
         <div className="ColorPicker">
             <h2>Color Picker</h2>
+            <HuePicker/>
         </div>
     );
 }
